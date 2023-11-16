dfea82e3ce3ff2270d25757ce4ceae0be262567d
rumlerjo <53153092+rumlerjo@users.noreply.github.com>
2023-11-15 19:11:19 -0500

    commit name changes

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sqlite3
 from bot import Bot
 import interactions
 
-db = sqlite3.connect("koneko.db")
+db = sqlite3.connect("mallard.db")
 
 client = interactions.Client(token=open("token.txt", "r").read())
 bot = Bot(client, db)
